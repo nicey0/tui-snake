5810af61e32d9037c6d6092c2b7221c50a9bda49
nicey0 <nicey@protonmail.ch>
2020-06-13 15:29:21 -0300

    Moved snake stuff to outside main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import curses
 
 
+snake: list = [(0, 0)]  # head is [0], body is [1:]. the snake
+for x in range(1, 11):
+    snake.append((0, x))
+
+
 def main(scr: curses.window):
     curses.curs_set(0)
-    snake: list = [(0, 0)]  # head is [0], body is [1:]. the snake
-    for x in range(1, 11):
-        snake.append((0, x))
     # is made of (y, x) pairs.
     for y, x in snake[1:]:
         scr.addstr(y, x, '@')
