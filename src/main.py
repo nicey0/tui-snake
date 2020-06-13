f7448aaaa8987969c872925d1fd89f3c78333375
nicey0 <nicey_pm@protonmail.com>
2020-06-13 15:23:31 -0300

    Snake display

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import curses
+
+
+def main(scr: curses.window):
+    curses.curs_set(0)
+    snake: list = [(0, 0)]  # head is [0], body is [1:]. the snake
+    for x in range(1, 11):
+        snake.append((0, x))
+    # is made of (y, x) pairs.
+    for y, x in snake[1:]:
+        scr.addstr(y, x, '@')
+    scr.addstr(snake[0][0], snake[0][1], '#')
+    scr.refresh()
+    scr.getch()
+
+
+if __name__ == '__main__':
+    curses.wrapper(main)
