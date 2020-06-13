ef14b37e12501e1e4cb1e9497d26624c54a9ea8d
nicey0 <nicey_pm@protonmail.com>
2020-06-13 15:36:47 -0300

    go_right and go_left

--- src/main.py
+++ src/main.py
@@ -1,9 +1,36 @@
 import curses
 
 
+NORTH = (-1, 0)
+SOUTH = (1, 0)
+EAST = (0, 1)
+WEST = (0, -1)
+
 snake: list = [(0, 0)]  # head is [0], body is [1:]. the snake
 for x in range(1, 11):
     snake.append((0, x))
+
+
+def go_left(direction: tuple) -> tuple:
+    if direction == NORTH:
+        return EAST
+    elif direction == SOUTH:
+        return WEST
+    elif direction == EAST:
+        return NORTH
+    elif direction == WEST:
+        return SOUTH
+
+
+def go_right(direction: tuple) -> tuple:
+    if direction == NORTH:
+        return WEST
+    elif direction == SOUTH:
+        return EAST
+    elif direction == EAST:
+        return SOUTH
+    elif direction == WEST:
+        return NORTH
 
 
 def main(scr: curses.window):
