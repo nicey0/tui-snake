bfe803eb49b7b807bd71513e68905c14995eee54
nicey0 <nicey@protonmail.ch>
2020-06-13 16:46:35 -0300

    snake tick

--- src/main.py
+++ src/main.py
@@ -7,8 +7,18 @@ EAST = (0, 1)
 WEST = (0, -1)
 
 snake: list = [(0, 0)]  # head is [0], body is [1:]. the snake
-for x in range(1, 11):
-    snake.append((0, x))
+direction = EAST
+
+
+def tick(snake: list, direction: tuple, key: int) -> (list, tuple):
+    if key == ord('j'):
+        direction = go_left(direction)
+    elif key == ord('k'):
+        direction = go_right(direction)
+    for i in range(1, len(snake))[::-1]:
+        snake[i] = snake[i-1]
+    snake[0] = (snake[0][0] + direction[0],
+                snake[0][1] + direction[1])
 
 
 def go_left(direction: tuple) -> tuple:
@@ -33,14 +43,19 @@ def go_right(direction: tuple) -> tuple:
         return NORTH
 
 
-def main(scr: curses.window):
-    curses.curs_set(0)
-    # is made of (y, x) pairs.
+def draw_snek(scr: curses.window, snake: list):
     for y, x in snake[1:]:
         scr.addstr(y, x, '@')
     scr.addstr(snake[0][0], snake[0][1], '#')
-    scr.refresh()
-    scr.getch()
+
+
+def main(scr: curses.window):
+    curses.curs_set(0)
+    scr.nodelay(True)
+    while True:
+        draw_snek(scr, snake)
+        tick(snake, direction, scr.getch())
+        scr.refresh()
 
 
 if __name__ == '__main__':
